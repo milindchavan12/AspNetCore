d742784ebe33613f58da5117dcdeff977f3e4701
milindchavan12 <milindchavan12@gmail.com>
2018-10-17 22:49:18 +0200

    Added Cities Controller

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        public CitiesController()
+        {
+        }
+
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {id = 1, Name = "NewYork"},
+                new {id = 2, Name = "Mumbai"}
+            });
+        }
+    }
+}
